ca5f30f5ff9f8e7b7df415e3f4142d3597b97b98
whimsical-phoenix <shahprapti12@gmail.com>
2023-12-18 23:02:55 -0700

    changed twilio number

--- server/server.js
+++ server/server.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const twilio = require('twilio');
+const express = require("express");
+const twilio = require("twilio");
 
 const app = express();
 const port = 3001; // or any other port you prefer
 
 // Twilio credentials
-const accountSid = 'AC5063628bd2360c9dcee6e2e75f2f0445';
-const authToken = 'f2cf4ec4ea937e78795ae2dcb5f2f830';
-const twilioPhoneNumber = '+15086391090';
+// const accountSid = "AC5063628bd2360c9dcee6e2e75f2f0445";
+// const authToken = "f2cf4ec4ea937e78795ae2dcb5f2f830";
+// const twilioPhoneNumber = "+15086391090";
 
 const client = twilio(accountSid, authToken);
 
 // Endpoint to trigger a phone call
-app.get('/call-help', (req, res) => {
-  const toPhoneNumber = '+14039185826'; // Replace with the actual phone number
+app.get("/call-help", (req, res) => {
+  const toPhoneNumber = "+14036201992"; // Replace with the actual phone number
 
   // Initiate a call
   client.calls
     .create({
-      url: 'http://demo.twilio.com/docs/voice.xml', // Replace with a TwiML URL or other endpoint
+      url: "http://demo.twilio.com/docs/voice.xml", // Replace with a TwiML URL or other endpoint
       to: toPhoneNumber,
       from: twilioPhoneNumber,
     })
-    .then(call => {
+    .then((call) => {
       console.log(`Call SID: ${call.sid}`);
-      res.send('Call initiated!');
+      res.send("Call initiated!");
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
-      res.status(500).send('Error initiating the call');
+      res.status(500).send("Error initiating the call");
     });
 });
 
